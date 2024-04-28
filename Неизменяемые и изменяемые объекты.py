5c30131d0c20436db6dcaa838fd3839b04601b2d
Whale-99 <168347567+Whale-99@users.noreply.github.com>
2024-04-29 01:33:46 +0300

    Add files via upload

---
+++ Неизменяемые и изменяемые объекты.py
@@ -0,0 +1,6 @@
+immutable_var = "Текст", "и", 2, 7, 10
+print(immutable_var)
+# Кортеж - не изменяемая коллекция. Поэтому нельзя изменить значение элементов кортежа
+mutable_list = ["Текст", "и", 2, 7, 10]
+mutable_list[0] = "Буквы"
+print(mutable_list)
